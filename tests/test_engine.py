93d25946969e1b3aff0de969e180f2cec3ca5ba6
Marion McWilliams <marionlmcwilliams@gmail.com>
2026-03-30 20:13:15 -0400

    Adds test for hands with ace and create fn to calc hand value

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,4 +1,4 @@
-from src.engine import Card, Rank, Suit
+from src.engine import Card, Rank, Suit, calculate_hand_value
 
 
 def test_card():
@@ -10,3 +10,30 @@ def test_card():
 
     # Assert
     assert card_value == 11
+
+
+def test_hand_value():
+    # Arrange
+    card1 = Card(Rank.KING, Suit.SPADES)
+    card2 = Card(Rank.KING, Suit.HEARTS)
+    hand = [card1, card2]
+
+    # Act
+    hand_value = calculate_hand_value(hand)
+
+    # Assert
+    assert hand_value == 20
+
+
+def test_hand_value_with_ace():
+    # Arrange
+    card1 = Card(Rank.KING, Suit.SPADES)
+    card2 = Card(Rank.FIVE, Suit.HEARTS)
+    card3 = Card(Rank.ACE, Suit.HEARTS)
+    hand = [card1, card2, card3]
+
+    # Act
+    hand_value = calculate_hand_value(hand)
+
+    # Assert
+    assert hand_value == 16
